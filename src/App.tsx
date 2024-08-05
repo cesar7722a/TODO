289de9fd7fa2680cfd0a6ce7bc2742fde7e8805b
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-05 22:54:01 +0100

    adiionei estilo na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { CirclePlus } from "lucide-react"
+import { CirclePlus, ClipboardList } from "lucide-react"
 
 function App() {
 
@@ -13,12 +13,38 @@ function App() {
     <input 
     type="text"
     placeholder="Adicionar uma nova tarefa"
-    className="border w-[618px] rounded-lg px-4 py-2 bg-[#F0EDF2] outline-none focus:border-[#9359F3]"
+    className="border w-[618px] rounded-lg px-4 py-2
+    bg-[#F0EDF2] outline-none focus:border-[#9359F3] border-[#E0DCE4]"
     />
-    <button className="bg-[#6F3CC3] font-bold px-6 py-2.5 rounded-lg flex gap-2 text-[#F0EDF2]">
+    <button 
+    className="hover:bg-[#9359F3] bg-[#6F3CC3] font-bold px-6 py-2.5 rounded-lg flex gap-2 text-[#F0EDF2]">
       <span>Criar</span>
       <CirclePlus />
     </button>
+   </div>
+   <div className="w-[736px] mt-8 space-y-8">
+    <div className="flex justify-between">
+      <span className="flex gap-2 items-center text-[#6B6572] ">
+        Tarefas Criadas 
+        <span className="px-1.5 flex justify-center items-center bg-[#DDD2EF] text-[#6F3CC3] rounded-full font-bold">
+          344
+        </span>
+      </span>
+      <span className="flex gap-2 items-center text-[#6B6572]">
+        Concluídas
+        <span className="px-1.5 flex justify-center items-center bg-[#BFE3D0] text-[#2D6C4A] rounded-full font-bold">
+          344
+        </span>
+      </span>
+    </div>
+    <div className="h-px bg-[#E0DCE4]"/>
+    <div>
+       <div>
+        <ClipboardList className="w-9 h-11 text-[#DDD2EF] mx-auto" />
+        <p className="text-[#6B6572] font-semibold text-center">Você ainda não tem tarefas cadastradas</p>
+        <p className="text-[#6B6572] text-center">Crie tarefas e organize seus itens a fazer</p>
+       </div>
+    </div>
    </div>
   </div>
 }
